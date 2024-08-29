3835e5c6421af90f53abdb855164e9fb046b569d
MellGriffin <missmelissagriffin@gmail.com>
2024-08-29 11:36:20 -0500

    Got nav bar styled

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,10 +1,12 @@
+import './Landing.css';
+
 const Landing = (props) => {
 
   const landingItems = props.items.map(item => <li>{item.name}</li>)
   
   return (
     <main>
-      <h1>Bag of Holding</h1>
+      <h1 id="page-title">Bag of Holding</h1>
       <ul id="landing-list">
       {landingItems.slice(-3)}
       </ul>
